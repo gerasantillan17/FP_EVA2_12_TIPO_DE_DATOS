8b341ea25ff99269be8f7cafc881ef80f5da928c
gerasantillan17 <gerapumas122@gmail.com>
2024-11-07 00:04:26 -0600

    Add files via upload

---
+++ JavaApplication53.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication53;
+
+/**
+ *
+ * @author gerar
+ */
+public class JavaApplication53 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       byte valor=-128;
+       byte valor2= 127;
+        System.out.println("Valor ="+ (valor));
+        System.out.println("Valor2="+ (valor));
+       short val=-32768;
+       short val2= 32767;
+       int x = -2147483618;
+       int y = -2147483617;
+    }
+    
+}
